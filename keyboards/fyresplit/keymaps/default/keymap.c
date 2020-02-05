2873b40f42ad10bd80b7b1da52293ebec0b4537b
Albert Luu <albert.luu.251@gmail.com>
2020-02-04 18:00:55 -0700

    Update keymap.c

--- keyboards/fyresplit/keymaps/default/keymap.c
+++ keyboards/fyresplit/keymaps/default/keymap.c
@@ -27,6 +27,7 @@
 #define _SWAP 12
 #define _SWAPK 11
 #define _GAME 10
+#define _FNSPC 9
 
 
 // Keynames
@@ -35,17 +36,28 @@
 #define T_CDT TG(_CADET)
 #define T_GAME TG(_GAME)
 #define T_SWAP TG(_SWAP)
+#define T_FSPC TG(_FNSPC)
 #define B_SSPC LT(_SWAPK, KC_SPC)
+#define B_FSPC LT(_FN, KC_SPC)
 #define ALT_TAB LALT(KC_TAB)
+#define CTL_TAB LCTL(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ortho( /* Base */
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,  \
     KC_PAUS, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
     ALT_TAB, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-    KC_UP,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
-    KC_DOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+    CTL_TAB, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+    KC_NO,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
     B_FEAT,  KC_LCTL, KC_LGUI, KC_LALT, B_FN,    KC_SPC,                    KC_SPC,  B_FN,    KC_HOME, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT  \
+  ),
+  [_FNSPC] = LAYOUT_ortho( /* Space Function Key Feature Toggle Layer */
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, B_FSPC ,                   B_FSPC , _______, _______, _______, _______, _______, _______  \
   ),
   [_GAME] = LAYOUT_ortho( /* Game mode Layer */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -81,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_FEAT] = LAYOUT_ortho( /* Feature Toggle Layer */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, T_GAME,  T_CDT  , T_SWAP , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, T_GAME,  T_CDT,   T_SWAP,  T_FSPC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
