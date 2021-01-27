d3ebf91befa31535b5b41504a4e6a8b2372b120e
Albert Luu <albert.luu.251@gmail.com>
2021-01-27 09:20:26 -0700

    Update Keymap

--- keyboards/fyresplit2/fyresplit2.h
+++ keyboards/fyresplit2/fyresplit2.h
@@ -27,10 +27,24 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
+    L00, L01, L02, L03, L04, L05, L06, R00, R01, R02, R03, R04, R05, R06, R07, R08, \
+    L10, L11, L12, L13, L14, L15, L16, R10, R11, R12, R13, R14, R15,      R17, R18, \
+    L20, L21, L22, L23, L24,      L26, R20, R21, R22, R23, R24, R25, R26, R27, R28, \
+    L30, L31, L32, L33, L34,      L36, R30, R31, R32, R33, R34, R35,      R37, R38, \
+    L40,      L42, L43, L44, L45, L46, R40, R41, R42, R43, R44,      R46, R47, R48, \
+    L50, L51, L52, L53,      L55,           R51,      R53, R54,      R56, R57, R58  \
 ) \
 { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    { L00,   L01,   L02,   L03,   L04,   L05,   L06  , KC_NO, KC_NO}, \
+    { L10,   L11,   L12,   L13,   L14,   L15,   L16  , KC_NO, KC_NO}, \
+    { L20,   L21,   L22,   L23,   L24,   KC_NO, L26  , KC_NO, KC_NO}, \
+    { L30,   L31,   L32,   L33,   L34,   KC_NO, L36  , KC_NO, KC_NO}, \
+    { L40,   KC_NO, L42,   L43,   L44,   L45,   L46  , KC_NO, KC_NO}, \
+    { L50,   L51,   L52,   L53,   KC_NO, L55,   KC_NO, KC_NO, KC_NO}, \
+    { R00,   R01,    R02,   R03,   R04,   R05,   R06,   R07,   R08   }, \
+    { R10,   R11,    R12,   R13,   R14,   R15,   KC_NO, R17,   R18   }, \
+    { R20,   R21,    R22,   R23,   R24,   R25,   R26,   R27,   R28   }, \
+    { R30,   R31,    R32,   R33,   R34,   R35,   KC_NO, R37,   R38   }, \
+    { R40,   R41,    R42,   R43,   R44,   KC_NO, R46,   R47,   R48   }, \
+    { KC_NO, R51,    KC_NO, R53,   R54,   KC_NO, R56,   R57,   R58   }  \
 }
